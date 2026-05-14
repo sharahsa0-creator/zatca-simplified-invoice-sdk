bb01af00e2e768e974ad2f1a1347ac847de1a824
sharahsa0-creator <sharahsa0@gmail.com>
2026-05-14 18:20:29 +0300

    Add test helpers

---
+++ tests/helpers.ts
@@ -0,0 +1,9 @@
+import type { ZatcaQRCodeData } from '../src/index.js';
+
+export const sampleQRCodeData: ZatcaQRCodeData = {
+  sellerName: 'Store',
+  vatRegistrationNumber: '300000000000003',
+  timestamp: '2025-01-01T00:00:00Z',
+  invoiceTotal: '100.00',
+  vatTotal: '15.00'
+};
